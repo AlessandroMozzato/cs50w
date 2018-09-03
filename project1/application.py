2e5deb1358af8e46757bc2ccb62ce4429a594908
AlessandroMozzato <mozzatoale@gmail.com>
2018-09-03 22:53:41 +0200

    added search and book

--- project1/application.py
+++ project1/application.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
 
 app = Flask(__name__)
 
@@ -25,3 +26,35 @@ grkey = 'O5rPbEJ5Y2O1jbWJppuBA'
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+@app.route("/search/", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        title = request.form.get("title")
+        print(title)
+        title = "%"+title+"%"
+        books = db.execute("""
+                    SELECT * 
+                    FROM books 
+                    WHERE 
+                        (title LIKE :title)
+                        OR (author LIKE :title)
+                        OR (isbn LIKE :title)
+                    """, {"title":title}).fetchall() 
+
+        if len(books) == 0:
+            return render_template("error.html", message="No books match the title author or isbn.")
+        else:
+            return render_template("search.html", books=books)       
+    return render_template("search.html", books=None)
+
+@app.route("/book/<int:book_id>")
+def book(book_id):
+    book = db.execute("SELECT * FROM books WHERE id = :id", {"id": book_id}).fetchone()
+    if book is None:
+        return render_template("error.html", message="No such book.")
+
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": grkey, "isbns": book.isbn})
+    reviews_count = res.json()['books'][0]['reviews_count']
+    average_rating = res.json()['books'][0]['average_rating']
+    return render_template("book.html", book=book, reviews_count=reviews_count, average_rating=average_rating)
