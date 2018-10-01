817be2dc587ff44f70490422b4fb0ed84b606a63
AlessandroMozzato <mozzatoale@gmail.com>
2018-10-01 22:20:28 +0200

    project 2 chat creation

--- project2/application.py
+++ project2/application.py
@@ -1,13 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+chats = {"new": 0}
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template('index.html', chats=chats)
+
+@socketio.on("submit vote")
+def vote(data):
+    selection = data["selection"]
+    chats[selection] += 1
+    emit("vote totals", chats, broadcast=True)
