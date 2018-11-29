3ff785160a94dc943c7c3249f6967e65edcc09af
AlessandroMozzato <mozzatoale@gmail.com>
2018-11-29 23:10:02 +0100

    first db p3

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    class_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    num_toppings = models.CharField(max_length=30)
+    size = models.BooleanField(default=True)
+    price = models.IntegerField()
+    price_big = models.IntegerField(null=True)
--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Menu
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+	context = {
+		"items" : Menu.objects.all()
+	}
+	return render(request, "orders/index.html", context)
