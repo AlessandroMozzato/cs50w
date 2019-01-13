0bf7eebccee1a340fe2a62e0ed140da8594ca371
AlessandroMozzato <mozzatoale@gmail.com>
2019-01-13 23:16:17 +0100

    fixed the javascript

--- project3/orders/static/orders/index.js
+++ project3/orders/static/orders/index.js
@@ -1,52 +1,58 @@
-// cart = {};
-
-cart = {};
+var cart = new Object();
+elements = 0;
 total = 0;
 
-toppings = ['Toppings', 'Pepperoni', 'Sausage', 'Mushrooms', 
-'Onions', 'Ham', 'Canadian Bacon', 'Pineapple', 'Eggplant', 
-'Tomato & Basil', 'Green Peppers','Hamburger', 'Spinach', 
-'Artichoke', 'Buffalo Chicken', 'Barbecue Chicken','Anchovies', 
-'Black Olives', 'Fresh Garlic', 'Zucchini'];
+toppings = ['Toppings', 'Pepperoni', 'Sausage', 'Mushrooms', 'Onions', 'Ham', 'Canadian Bacon', 'Pineapple', 
+'Eggplant', 'Tomato & Basil', 'Green Peppers','Hamburger', 'Spinach', 'Artichoke', 'Buffalo Chicken', 
+'Barbecue Chicken','Anchovies', 'Black Olives', 'Fresh Garlic', 'Zucchini'];
 
 function addToCart(class_name, name, price, num_toppings, size) {
-	// cart.append(item.name)
+    elements += 1;
+	cart[elements] = new Object();
 
     const li = document.createElement('li');
+    cart[elements]['class_name'] = class_name;
+    cart[elements]['name'] = name;
+    cart[elements]['size'] = size;
+    cart[elements]['price'] = price;
+    cart[elements]['tops'] = parseInt(num_toppings);
+    cart[elements]['curr_top'] = 0;
+
     li.innerHTML =  class_name+' '+name+' ('+size+') '+price;
     document.querySelector('#cartitems').append(li);
-    var twoPlacedFloat = + parseFloat(price)
+    var twoPlacedFloat = + parseFloat(price);
     total = total + twoPlacedFloat;
-    text = "The current total is: "+total.toFixed(2).toString()
+    text = "The current total is: "+total.toFixed(2).toString();
+	
     document.querySelector('#total').innerHTML = text;
-
-    var tops = parseInt(num_toppings);
-
-	while (tops > 0) {
-		addToppings(class_name);
-		tops -= 1;
+    
+    if (parseInt(num_toppings) > 0) {
+		addToppings(class_name, elements);
 	}
 }
 
-function addToppings(class_name) {
+function addToppings(class_name, el) {
 	for (var i = toppings.length - 1; i >= 0; i--) {
 		const li = document.createElement('li');
 		const aa = document.createElement('a');
-
     	aa.innerHTML = toppings[i];
     	aa.href = "#";
-    	// aa.onclick = addTop(toppings[i]);
+
+    	aa.onclick = function() {
+            var top = this.innerHTML;
+            var text = document.querySelector('#cartitems').lastElementChild.innerHTML;
+            document.querySelector('#cartitems').lastElementChild.innerHTML = text + ", " + top;
+            cart[el]['curr_top'] += 1;
+
+            if (cart[el]['tops'] == cart[elements]['curr_top']) {
+                var ul = document.getElementById("toppings");
+                while(ul.firstChild){
+                    ul.removeChild(ul.firstChild);
+                }
+            }
+        }
+
     	li.appendChild(aa);
     	document.querySelector('#toppings').append(li);
 	}
-}
-
-function addTop(val) {
-	var lii = document.querySelector('#cartitems').lastElementChild;
-	lii.innerHTML = val;
-
-	 var ul = document.getElementById("toppings");
-
-     while (ul.firstChild)
-         ul.removeChild(ul.firstChild);
 }
